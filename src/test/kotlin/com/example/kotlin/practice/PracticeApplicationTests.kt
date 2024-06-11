c31cc4a624f27053933fa12e476bc63d5a7e5128
Chung Lee <s2econd.blue@gmail.com>
2024-06-12 05:49:39 +0900

    first commit

---
+++ src/test/kotlin/com/example/kotlin/practice/PracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlin.practice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
